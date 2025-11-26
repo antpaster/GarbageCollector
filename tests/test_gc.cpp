88a1aeb07901c96fc218b7945fb183f101e09e2c
Anton Pasternak <antpaster@gmail.com>
2025-11-26 14:10:27 +0300

    Add: a couple of Google tests.

---
+++ tests/test_gc.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "gc_manager.hpp"
+#include "gc_object.hpp"
+
+struct TestObj : public GCObject {
+    TestObj(GCManager& gc) : GCObject(gc) {}
+};
+
+TEST(GCSimple, SingleDeletion) {
+    GCManager gc;
+
+    auto* a = new TestObj(gc);
+    a->decref(); // should be deleted
+
+    gc.collect_cycles();
+
+    SUCCEED();
+}
+
+TEST(GCCycles, DetectCycle) {
+    GCManager gc;
+
+    auto* a = new TestObj(gc);
+    auto* b = new TestObj(gc);
+
+    a->add_ref(b);
+    b->add_ref(a);
+
+    a->decref();
+    b->decref();
+
+    gc.collect_cycles();
+
+    SUCCEED();
+}
