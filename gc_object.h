fe118e5b7a7019927c7c834fcbd43ea0ccbc3057
Anton Pasternak <antpaster@gmail.com>
2025-11-26 07:45:06 +0300

    Upd: GC object.

--- gc_object.h
+++ gc_object.h
@@ -1,36 +1,43 @@
 #pragma once
-#include <vector>
-#include <iostream>
+#include <functional>
 
+// Base GCObject class
 class GCObject {
 public:
-    int refcount = 0;
-    std::vector<GCObject*> refs;
-
+    GCObject() = default;
     virtual ~GCObject() = default;
 
-    void add_ref(GCObject* obj) {
-        if (obj) {
-            refs.push_back(obj);
-            obj->incref();
-        }
+    // Intrusive refcount: starts at 0; owners (roots or other objects) call incref.
+    int refcount{0};
+
+    // For cycle detection bookeeeping
+    int gc_refs{0};
+
+    // Whether the object can hold references to other GC objects and should be tracked
+    bool tracked{false};
+
+    // By default no finalizer
+    virtual bool has_finalizer() const { return false; }
+    virtual void finalize() {}
+
+    // Subclasses that contain pointers must implement traversal:
+    // call visitor(child) for each child pointer (may be null).
+    virtual void traverse_references(const std::function<void(GCObject*)>& visitor) {
+        (void)visitor;
     }
 
+    // Helpers
     void incref() {
-        refcount++;
-    }
-    void decref() {
-        if (--refcount == 0) {
-            destroy();
-        }
+        ++refcount;
     }
 
-protected:
-    virtual void destroy() {
-        for (auto ref : refs) {
-            ref->decref();
+    void decref() {
+        --refcount;
+        if (refcount == 0) {
+            // immediate destruction (non-cyclic case)
+            // Ensure unregister before delete to keep manager consistent.
+            GCManager::instance().unregister_object(this);
+            delete this;
         }
-        refs.clear();
-        delete this;
     }
 };
